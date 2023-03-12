a1a200145aaf375c04bf185d822c9de6756221ee
Lucas Maclean <contatolucasmaclean@gmail.com>
2023-03-12 18:21:58 -0300

    Segunda página finalizada com sucesso, renderização dos cards via JavaScript

--- js/maiorIdadeRenderUs.js
+++ js/maiorIdadeRenderUs.js
@@ -1,9 +1,9 @@
 
 
 const dataBaseProducts = [
-    { name: 'Vinho Tinto', price: 80, button: 'COMPRAR', img: '../img/vinho.svg' },
-    { name: 'Cerveja Heineken', price: 20, button: 'COMPRAR', img: '../img/heineken.svg' },
-    { name: 'Guaraná Antártica', price: 10, button: 'COMPRAR', img: '../img/refri.svg' },
+    { id: 1, name: 'Vinho Tinto', price: 80, button: 'COMPRAR', img: '../img/vinho.svg' },
+    { id: 2, name: 'Cerveja Heineken', price: 20, button: 'COMPRAR', img: '../img/heineken.svg' },
+    { id: 3, name: 'Guaraná Antártica', price: 10, button: 'COMPRAR', img: '../img/refri.svg' },
 
 ];
 
@@ -13,7 +13,7 @@ const ulContainer = document.getElementById("ulProducts");
 
 function renderProducts(obj) {
 
-    obj.forEach(element => {
+    obj.forEach((element, i) => {
 
         let liCard = document.createElement("li");
         liCard.classList = "boxCard";
@@ -24,8 +24,11 @@ function renderProducts(obj) {
         let titleCard = document.createElement("h2");
         let priceCard = document.createElement("p");
         let buttonCard = document.createElement("button");
-        buttonCard.setAttribute("id", "redirect")
-        buttonCard.setAttribute("onclick", "redirect()") // Chamando a função de redirect
+        buttonCard.setAttribute("id", `${i}`);
+
+        buttonCard.addEventListener("click", () => {
+            location.href = `../pages/maiorIdadeTotal.html?id=${i}`
+        });
 
         imgCard.src = element.img;
         titleCard.innerText = element.name;
@@ -33,12 +36,8 @@ function renderProducts(obj) {
         buttonCard.innerText = element.button;
 
         liCard.append(imgCard, titleCard, priceCard, buttonCard)
-        ulContainer.appendChild(liCard)
+        ulContainer.appendChild(liCard);
 
     });
-}
+};
 renderProducts(dataBaseProducts);
-
-
-let buttonCardSelect = document.getElementById("redirect");
-buttonCardSelect.addEventListener
